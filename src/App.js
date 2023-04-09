d8a2f8278df9236e3196ecc264c7e555aa8868f1
Wei Shen Tan <tanweishen97@gmail.com>
2023-04-09 18:05:41 +0800

    Implemented mesh triangle disintegration

--- src/App.js
+++ src/App.js
@@ -1,38 +1,116 @@
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { useEffect } from 'react';
+import { BufferAttribute } from 'three';
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+function DisintegrationMesh() {
+  const geometryRef = useRef(null);
+  const shaderRef = useRef(null);
+
+  useEffect(() => {
+    const count = geometryRef.current.attributes.position.count;
+    let randoms = new Float32Array(count);
+    for (let i = 0; i < count; i+=3) {
+      const r = Math.random();
+      randoms[i] = r;
+      randoms[i + 1] = r;
+      randoms[i + 2] = r;
+    }
+    geometryRef.current.setAttribute('aRandom', new BufferAttribute(randoms, 1));
+
+  }, []);
+
+  const uniforms = {
+    uTime: {
+      value: 0.0,
+    }
+  };
+
+  const vertexShader = `
+    varying vec2 vUv;
+    attribute float aRandom;
+    uniform float uTime;
+    varying float vSinValue;
+
+    mat4 rotation3d(vec3 axis, float angle) {
+      axis = normalize(axis);
+      float s = sin(angle);
+      float c = cos(angle);
+      float oc = 1.0 - c;
+
+      return mat4(
+        oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+        oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+        oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+        0.0,                                0.0,                                0.0,                                1.0
+      );
+    }
+
+    vec3 rotate(vec3 v, vec3 axis, float angle) {
+      return (rotation3d(axis, angle) * vec4(v, 1.0)).xyz;
+    }
+
+    float easeIn(float k) {
+      return pow(k, 5.);
+    }
+
+    void main() {
+      vUv = uv;
+      float displacementFactor = 10.;
+      vec3 offset = (aRandom * normal) * 2.;
+      float sinValue = uTime * 0.5;
+      vSinValue = sinValue;
+      offset *= easeIn(abs(sin(sinValue))) * displacementFactor;
+
+      vec3 finalPosition = position + offset;
+
+      finalPosition = rotate(finalPosition, vec3(1., 1., 0.), (uTime * 0.5));
+      vec4 modelViewPosition = modelViewMatrix * vec4(finalPosition, 1.0);
+      gl_Position = projectionMatrix * modelViewPosition;
+    }
+  `;
+
+  const fragmentShader = `
+    varying vec2 vUv;
+    uniform float uTime;
+    varying float vSinValue;
+
+    float easeIn(float k) {
+      return pow(k, 10.);
+    }
+
+    void main() {
+      float opacity = 1.;
+      opacity -= easeIn(abs(sin(vSinValue))) ;
+      gl_FragColor = vec4(vUv, 1., opacity);
+    }
+  `;
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    shaderRef.current.uniforms.uTime.value = time;
+  });
+  return <>
+    <mesh>
+      <icosahedronGeometry ref={geometryRef} args={[1, 3]}/>
+      <shaderMaterial
+        ref={shaderRef}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        uniforms={uniforms}
+        wireframe
+        transparent
+      />
     </mesh>
-  )
+  </>
 }
 
 export default function App() {
   return (
     <Canvas>
       <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <DisintegrationMesh/>
       <OrbitControls />
     </Canvas>
   )
